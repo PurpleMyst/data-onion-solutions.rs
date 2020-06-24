869b7dbc491abd19408b1c0bd7a178384be9f665
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-06-24 14:10:16 +0200

    refactor: reduce allocations in layer 5

--- src/layer5.rs
+++ src/layer5.rs
@@ -1,4 +1,5 @@
 use std::convert::TryInto;
+use std::mem::size_of;
 
 use aes::Aes256;
 use block_modes::block_padding::NoPadding;
@@ -11,54 +12,60 @@ fn concat(a: u64, b: u64) -> u128 {
     ((a as u128) << 64) | (b as u128)
 }
 
-fn unwrap_key(mut ciphertext: Vec<u64>, kek: &[u8], iv: &[u8]) -> Vec<u64> {
+fn unwrap_key(ciphertext: &mut Vec<u64>, kek: &[u8], iv: &[u8]) {
+    // n is the amount of 64-bit blocks of the plaintext
     let n = (ciphertext.len() - 1) as u64;
 
     let mut a = ciphertext[0];
 
     for j in (0..=5).rev() {
-        for i in (1..=n).rev() {
-            let t = n * j + i;
+        for (i, c) in ciphertext.iter_mut().enumerate().skip(1).rev() {
+            let t = n * j + i as u64;
+            let mut block = concat(a ^ t, *c).to_be_bytes();
 
-            let b = u128::from_be_bytes(
-                Aes256Ecb::new_var(kek, iv)
-                    .expect("could not create AES")
-                    .decrypt_vec(&concat(a ^ t, ciphertext[i as usize]).to_be_bytes())
-                    .expect("no decrypt")
-                    .as_slice()
-                    .try_into()
-                    .unwrap(),
-            );
+            // decrypt this block in place
+            Aes256Ecb::new_var(kek, iv)
+                .expect("could not create AES")
+                .decrypt(&mut block)
+                .expect("could not decrypt block");
 
+            // separate it into its constituents
+            let b = u128::from_be_bytes(block[..].try_into().unwrap());
             a = (b >> 64) as u64;
-            ciphertext[i as usize] = (b & ((1 << 64) - 1)) as u64;
+            *c = (b & ((1 << 64) - 1)) as u64;
         }
     }
 
-    assert_eq!(a, u64::from_be_bytes(iv.try_into().unwrap()));
+    // if in debug mode, verify the IV
+    debug_assert_eq!(a, u64::from_be_bytes(iv.try_into().unwrap()));
 
-    ciphertext.into_iter().skip(1).collect()
+    // remove the extra block that shouldn't be part of the plaintext
+    ciphertext.remove(0);
 }
 
 pub fn solve(payload: Vec<u8>) -> Vec<u8> {
     let (header, payload) = payload.split_at(32 + 8 + 40 + 16);
     let kek = &header[0..32];
     let kek_iv = &header[32..32 + 8];
-    let encrypted_key = &header[32 + 8..32 + 8 + 40];
+    let key = &header[32 + 8..32 + 8 + 40];
     let payload_iv = &header[32 + 8 + 40..32 + 8 + 40 + 16];
 
-    let key = encrypted_key
-        .chunks(std::mem::size_of::<u64>())
+    // Parse they key into 64-bit blocks
+    let mut key = key
+        .chunks(size_of::<u64>())
         .map(|chunk| u64::from_be_bytes(chunk.try_into().unwrap()))
         .collect::<Vec<_>>();
 
-    let key = unwrap_key(key, kek, kek_iv);
+    // Apply the AES key unwrapping algorithm
+    unwrap_key(&mut key, kek, kek_iv);
 
-    let mut blarg_key = Vec::new();
-    for b in &key {
-        blarg_key.extend(&b.to_be_bytes())
-    }
+    // Then parse it back into bytes
+    let key = key.into_iter().fold(Vec::new(), |mut v, n| {
+        v.extend(&n.to_be_bytes()[..]);
+        v
+    });
 
-    let aes = Aes256Cbc::new_var(&blarg_key, payload_iv).unwrap();
+    // Now us the unwrapped key to decrypt the payload
+    let aes = Aes256Cbc::new_var(&key, payload_iv).unwrap();
     aes.decrypt_vec(payload).unwrap()
 }
